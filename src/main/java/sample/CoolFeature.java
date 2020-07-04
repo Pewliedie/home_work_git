7743f4016e3039fa18e0c727e7a61e9b243b457f
Vladislav Permyakov <vladislav_permyakov@epam.com>
2020-07-04 16:19:05 +0600

    Add function actionFromConflictBranch() in CoolFeature

--- src/main/java/sample/CoolFeature.java
+++ src/main/java/sample/CoolFeature.java
@@ -4,4 +4,8 @@ public class CoolFeature {
     void coolFunction(){
         System.out.println("updated cool function");
     }
+
+    void actionFromConflictBranch(){
+        System.out.println("action from conflict branch");
+    }
 }
